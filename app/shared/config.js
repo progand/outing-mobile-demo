de834888b7276c7094a7f1dbf53258196c4a4ffe
progand <lach@ukr.net>
2017-12-08 09:25:09 +0200

    [*] Removed app/*.js files from git

--- app/shared/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Config = (function () {
-    function Config() {
-    }
-    Config.apiUrl = "https://cn.outingtravel.com";
-    return Config;
-}());
-exports.Config = Config;
